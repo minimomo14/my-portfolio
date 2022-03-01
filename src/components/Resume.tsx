8717753dde7b42b2240df7248172db29a4c90934
Momo Miller <minimomo14z@hotmail.com>
2022-02-28 22:24:32 -0500

    updates content and styles

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -5,6 +5,7 @@ function Resume() {
         <div className="resume">
             <div className="resume-container">
             <h1>My Resume</h1>
+            <img src={`${process.env.PUBLIC_URL}/resume/my-resume.png`} alt="" />
             <a href={`${process.env.PUBLIC_URL}/resume/my-resume.pdf`} target="_blank">Click here to download my resume.</a>
             </div>
         </div>
